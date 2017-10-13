4d50b2faa1ca12721b598c70417ae1c688867607
Adrian Herrera <simplyadrian@gmail.com>
2017-10-13 18:20:43 +0000

    adding variable for region

--- restore.sh
+++ restore.sh
@@ -1,6 +1,6 @@
 if [[ -n "$PRODUCT" ]] && [[ ! -e $JENKINS_HOME/config.xml ]]
 then
-  aws --region us-west-2 s3 cp "s3://<some_bucket_name>/$PRODUCT/jenkins/jenkins_data.tgz" /tmp/restored.tgz &&\
+  aws --region $REGION s3 cp "s3://<some_bucket_name>/$PRODUCT/jenkins/jenkins_data.tgz" /tmp/restored.tgz &&\
   tar -xzf /tmp/restored.tgz -C /  &&\
   chown -R jenkins:jenkins $JENKINS_HOME &&\
   rm /tmp/restored.tgz || true

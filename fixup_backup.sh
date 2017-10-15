e702526b5eb60776f0864cbcc0b4217fda58df72
Adrian Herrera <simplyadrian@gmail.com>
2017-10-15 05:55:37 +0000

    making the bash prettier.

--- fixup_backup.sh
+++ fixup_backup.sh
@@ -1,4 +1,4 @@
-if [[ -n "$PRODUCT" && -n "$ROOT_BUCKET" ]]
+if [[ -n "$PRODUCT" ]] && [[ -n "$ROOT_BUCKET" ]]
 then
   sed -i.bak "s/REPLACEME1/$ROOT_BUCKET/g" $JENKINS_HOME/jobs/Management\ Jobs/jobs/backup_jenkins/config.xml
   sed -i.bak "s/REPLACEME2/$PRODUCT/g" $JENKINS_HOME/jobs/Management\ Jobs/jobs/backup_jenkins/config.xml
